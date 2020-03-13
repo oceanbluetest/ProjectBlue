c2957b2bdd6225e30f8a4dbec99e836e01af2796
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2020-03-13 07:35:42 -0400

    TestBase class is complete with report and logs

--- src/test/java/PHPTravels_Test.java
+++ src/test/java/PHPTravels_Test.java
@@ -1,12 +1,30 @@
 
+import Tests.TestBase;
+import com.relevantcodes.extentreports.LogStatus;
 import org.testng.annotations.Test;
-import util.ConfigReader;
 
-public class PHPTravels_Test extends TestBase{
+public class PHPTravels_Test extends TestBase {
 
-    @Test
-    public void test1(){
-        System.out.println(ConfigReader.readProperty("browser"));
+    @Test(description = "Amazon Test")
+    public void testAmazon(){
+       driver.get("https://amazon.com");
+        reporter.log(LogStatus.INFO,driver.getTitle());
+        screenshot.takeScreenshotAndLog();
+        //test.log(LogStatus.PASS, "Screenshot below: " + test.addBase64ScreenShot(Screenshot.takeScreenshot(driver, test)));
+    }
+
+    @Test(description = "Google Test")
+    public void testGoogle(){
+        driver.get("https://google.com");
+        reporter.log(LogStatus.INFO, driver.getTitle());
+        screenshot.takeScreenshotAndLog();
+    }
+
+    @Test(description = "TLA Test")
+    public void testTLA(){
+        driver.get("https://techleadacademy.io");
+        reporter.log(LogStatus.INFO, driver.getTitle());
+        screenshot.takeScreenshotAndLog();
     }
 
 
---
+++ src/test/java/Tests/TestBase.java
@@ -0,0 +1,84 @@
+package Tests;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+import util.Screenshot;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    public WebDriver driver;
+    public static final Logger logger = LogManager.getLogger("Log");
+    public ExtentReports extent;
+    public ExtentTest reporter;
+    public Screenshot screenshot;
+    public Test test;
+
+    @BeforeSuite
+    public void startReport() {
+        extent = new ExtentReports("target//extentReports-DARK.html", true);
+        extent
+                .addSystemInfo("Host Name", "TLA BootCamp")
+                .addSystemInfo("Environment", "Automation Testing")
+                .addSystemInfo("User name", "Kuba");
+        extent.loadConfig(new File("src/main/resources/extent-config.xml"));
+    }
+
+    @BeforeMethod
+    public void setUp(Method method){
+        test = method.getAnnotation(Test.class);
+        WebDriverManager.chromedriver().setup();
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");  //To get rid of console Warning outputs
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+
+        logger.info("STARTING test execution of Test: " + method.getName());
+
+        reporter = extent.startTest(test.description());  //extent.startTest((this.getClass().getSimpleName())); //can add description to Report here
+        //test.assignAuthor("Author name");
+        reporter.assignCategory(test.description());
+        screenshot = new Screenshot(driver, reporter);
+    }
+
+    @AfterMethod
+    public void tearDown(Method method, ITestResult result){
+        logger.info("ENDING test execution of Test: " + method.getName());
+        if(result.getStatus() == ITestResult.FAILURE) {
+            //test.log(Status.FAIL, MarkupHelper.createLabel(result.getName()+" FAILED ", ExtentColor.RED));
+            reporter.log(LogStatus.FAIL, result.getName(), "Test was Unsuccessful");
+            reporter.log(LogStatus.FAIL, result.getThrowable());
+        } else if(result.getStatus() == ITestResult.SUCCESS) {
+            reporter.log(LogStatus.PASS, result.getName(), "Test was successful");
+        }
+        else {
+            reporter.log(LogStatus.SKIP, result.getName(), "Test was SKIPPED");
+            //test.skip(result.getThrowable());
+            reporter.log(LogStatus.SKIP, result.getThrowable());
+        }
+        extent.endTest(reporter);
+        driver.close();
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        extent.flush();
+        extent.close();
+    }
+
+
+}
